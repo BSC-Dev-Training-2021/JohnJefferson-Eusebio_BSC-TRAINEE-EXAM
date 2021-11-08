14c9b34e3288bfa14ac3cf8a0a91741b245cc56d
JayEusebio <jjeusebio.tech@gmail.com>
2021-11-08 13:34:49 +0800

    New Update

---
+++ O-Shopping/php/headernav1.php
@@ -0,0 +1,18 @@
+
+<header id=header>
+    <nav class="navbar navbar-expand-lg nav-link navbar-dark bg-danger justify-content-around">
+        
+        <ul class="nav nav-pills mr-auto p-y-2">
+            <li role="presentation"><a href="" class="nav-item nav-link text-white text-center"><i class="fas fa-home"> Home</i></a></li>
+        </ul>
+       
+        <ul class="nav nav-pills p-y-2">
+            <li role="presentation"><a href="" class="nav-item nav-link text-white text-center"><i class="fas fa-sign-in-alt"></i> Sign-in</a></li>
+        </ul>
+
+        <ul class="nav nav-pills p-y-2">
+            <li role="presentation"><a href="registration.php" class="nav-item nav-link text-white text-center"><i class=""></i> Sign-up</a></li>
+        </ul>
+       
+    </nav>
+</header>
